341092390118c3d2a5694ab96ef8043f67adeaf1
markoswythor <76796419+markoswythor@users.noreply.github.com>
2021-02-09 15:15:03 -0300

    Update suporte.java

--- Android/suporte.java
+++ Android/suporte.java
@@ -1,9 +1,9 @@
 /*
- * Classname             (darkbot3.0)
+ * Classname             (mksbot3.0)
  *
  * Version information   (3.0)
  *
  * Date                  (06/12,15:35)
  *
- * author                (Dark YT)
+ * author                (Markos)
  * Copyright notice      (bot com suporte a Android/Java)
