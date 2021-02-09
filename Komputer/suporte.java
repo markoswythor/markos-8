9b35c0d2a52f1fce9af40c832c9a304dfa2530f5
markoswythor <76796419+markoswythor@users.noreply.github.com>
2021-02-09 15:21:01 -0300

    Update suporte.java

--- Komputer/suporte.java
+++ Komputer/suporte.java
@@ -1,5 +1,5 @@
 Pc = komputer * (darkbot3.0 + darkbot - dark)
            + 4 * bot; // PREFER
 
-Android = /0/android/suporte.java * (darkbot3.0 + dark
-                       - darkbot) + 4 * bot; // AVOID
+Android = /0/android/suporte.java * (mksbot3.0 + markos
+                       - markosbot) + 4 * bot; // AVOID
